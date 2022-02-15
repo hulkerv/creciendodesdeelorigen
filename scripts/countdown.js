2192728b3c5fb051fae61580f60d80f8cd732ae2
Hulker <vegat.kevinj@gmail.com>
2022-02-15 17:43:38 -0400

    creciendo-desde-el-origen-2

---
+++ scripts/countdown.js
@@ -0,0 +1,32 @@
+const getRemainTime = (deadline) => {
+  let now = new Date();
+  const remainTime = (new Date(deadline) - now + 1000) / 1000;
+  const remainSeconds = ("0" + Math.floor(remainTime % 60)).slice(-2);
+  const remainMinutes = ("0" + Math.floor((remainTime / 60) % 60)).slice(-2);
+  const remainHours = ("0" + Math.floor((remainTime / 3600) % 24)).slice(-2);
+  const remainDays = Math.floor(remainTime / (3600 * 24));
+
+  return {
+    remainTime,
+    remainSeconds,
+    remainMinutes,
+    remainHours,
+    remainDays,
+  };
+};
+
+const countdown = (deadline, elem, finalMessage) => {
+  const el = document.getElementById(elem);
+
+  const timerUpdate = setInterval(() => {
+    let time = getRemainTime(deadline);
+    el.innerHTML = `Comenzamos en : </br> ${time.remainDays}d : ${time.remainHours}h : ${time.remainMinutes}m : ${time.remainSeconds}s`;
+
+    if (time.remainTime <= 1) {
+      clearInterval(timerUpdate);
+      el.innerHTML = finalMessage;
+    }
+  }, 1000);
+};
+
+countdown("Jun 01 2022 10:00:00 GMT-0500", "clock", "PROXIMAMENTE")
